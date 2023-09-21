bd902468fcb24986d5303fd9905201b9b2f63206
Duy Luan <phamduyluan102001@gmail.com>
2023-09-21 23:47:41 +0700

    Setting Profile UI and upload Image

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import DesktopItem from "./DesktopItem";
 import { User } from "@prisma/client";
 import Avatar from "../Avatar";
+import SettingModal from "./SettingModal";
 
 interface DesktopSidebarProps{
     currentUser: User
@@ -14,7 +15,9 @@ const DesktopSidebar:React.FC<DesktopSidebarProps> = ({currentUser}) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between ">
+    <>
+        <SettingModal currentUser={currentUser} isOpen={isOpen} onClose={()=> setIsOpen(false)}/>
+        <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between ">
         <nav className="mt-4 flex flex-col justify-between">
             <ul role="list" className="flex flex-col items-center space-y-1">
                 {routes.map((item)=>(
@@ -35,6 +38,7 @@ const DesktopSidebar:React.FC<DesktopSidebarProps> = ({currentUser}) => {
             </div>
         </nav>
     </div>
+    </>
   )
 }
 
